85cd66f1281f423a3f8c0352aab11c90c495fb40
NikitaShaberov <99290177+NikitaShaberov@users.noreply.github.com>
2022-02-26 19:57:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,19 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+#include <math.h>
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	for (int i = 2; i < sqrt(value); i++)
+	{
+		if (value % i == 0)
+		{
+			return false;
+
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {

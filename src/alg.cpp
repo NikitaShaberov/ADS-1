189789eb740d476e031f3453e5e9942dbd38a4b3
NikitaShaberov <99290177+NikitaShaberov@users.noreply.github.com>
2022-02-26 20:12:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,15 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	for (int i = 2; i < sqrt(value); i++)
+	{
+		if (value % i == 0)
+		{
+			return false;
+
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {

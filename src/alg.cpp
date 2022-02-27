393660a417fceaee4078a45e0f2a1699aa478886
NikitaShaberov <99290177+NikitaShaberov@users.noreply.github.com>
2022-02-27 20:36:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <iostream>
 #include "alg.h"
+#include <ctime>
+using namespace std;
 
 
 bool checkPrime(uint64_t value) {
@@ -16,13 +19,39 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+
+	uint64_t n;
+	int nPrime;
+	cin >> n;
+	cout << nPrime(n);
+	uint64_t prime = 1;
+	if (n == 1) return 2;
+	for (int i = 1; i < n; i++)
+	{
+		prime += 2;
+		while (checkPrime(prime) == 0)
+		{
+			prime += 2;
+		}
+	}
+	return prime;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+if (value == 2) return 3;
+	 if (checkPrime(value) == 1) value += 2;
+	  if (value % 2 == 0) value += 1;
+	   while (checkPrime(value) == 0) value += 2;
+	    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t prime = 3;
+	uint64_t sum = 2;
+	if (hbound <= 2) return 0;
+	while (prime < hbound) {
+		if (checkPrime(prime) == 1) sum += prime;
+		prime += 2;
+	}
+	return sum;
 }
